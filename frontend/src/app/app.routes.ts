15733241c60df5162878caee458ae28005f7adbf
Thomas Boigner <thomasboigner@outlook.com>
2026-06-06 17:11:56 +0200

    Fix prettier

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { Routes } from '@angular/router';
-import {ChatList} from './pages/chat-list/chat-list';
+import { ChatList } from './pages/chat-list/chat-list';
 
 export const routes: Routes = [
   {
     path: '**',
     title: 'List chats',
-    component: ChatList
-  }
+    component: ChatList,
+  },
 ];
